794a5a8a90fe211ac23c82dea6fe506665d4b96c
Langa <72982019+LangaXulu@users.noreply.github.com>
2020-11-02 11:04:29 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -46,7 +46,7 @@ namespace WindowsFormsApp2
             MyTimer.Stop();
             lblScore.Text += " Game over!!!";
         }
-        private void gameTimerEven(object sender, EventArgs e)
+        private void gameTimerEvent(object sender, EventArgs e)
         {
             FlappyBird.Top += gravity; 
             PipeUp.Left -= PipeSpeed; 
